f99efed8e1e2f4e9ad4cac1c428b26eff674e2c1
Rasyakr05 <159267203+Rasyakr05@users.noreply.github.com>
2024-11-03 17:09:29 +0530

    init

---
+++ backend/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const userRouter = require("./src/routes/users.js");
+const incomeRouter = require("./src/routes/income.js");
+const expenseRouter = require("./src/routes/expense.js");
+
+app.use("/api/auth", userRouter);
+app.use("/api/add", incomeRouter);
+app.use("/api/add", expenseRouter);
+
+// Serve static files
+// app.use(express.static(path.join(__dirname, '../build')));
+
+// app.get('/*', function(req, res) { 
+//     res.sendFile(path.join(__dirname, '../build', 'index.html')); 
+// });
+
+
+
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://rovxn:KEnvYxu0eKutHCAj@cluster0.8jgbr.mongodb.net/")
+
+  .then(() => {
+    // Start the server
+    app.listen(3001, () => console.log('Server listening on port 3001'));
+  })
+  .catch(error => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
+
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const path = require("path");
+// const app = express();
+
+
+// app.use(express.json());
+// app.use(cors());
+
+// // app.use("/auth", userRouter);
+// // app.use("/add", incomeRouter);
+// // app.use("/add", expenseRouter);
+
+// app.use("/api/auth", require("./routes/users.js"));
+// app.use("/api/add", require("./routes/income.js"));
+// app.use("/api/add", require("./routes/expense.js"));
+
+
+// app.use(express.static(path.join(__dirname,'/build')));
+
+// app.get('/*', function(req, res) { 
+//     res.sendFile(path.join(__dirname 
+//     ,'/build/index.html')); }); 
+
+// mongoose.connect("mongodb+srv://123:123@cluster0.pvpoxvw.mongodb.net/Cluster0?retryWrites=true&w=majority");
+
+// app.listen(3001,()=> console.log('server listening on'));
